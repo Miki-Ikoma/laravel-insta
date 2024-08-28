46c326dcbae3123b44daf0a27031d0a2c5111c68
生駒写真 <ikoma-photo@MacBook-Pro.local>
2024-08-28 22:00:23 +0900

    removed vite in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="{{ asset('css/app-D-sv12UV.css') }}">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
     <script src="{{ asset('js/app-DlPQVG7t.js') }}"></script>
 </head>
